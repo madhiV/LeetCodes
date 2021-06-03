1e615e662365b62ab1c4cd56809eb6e995fe5c0b
Madhivadhanan B R <72497080+madhiV@users.noreply.github.com>
2021-06-03 09:23:03 +0530

    After applying memoization

--- 97. Interleaving String.java
+++ 97. Interleaving String.java
@@ -1,24 +1,40 @@
 class Solution {
     public boolean isInterleave(String s1, String s2, String s3) {
-        if((s1.length()+s2.length())!=s3.length()){
+        if(s1.length()+s2.length()!=s3.length()){
             return false;
         }
-        return func(s1,s2,s3,0,0,0);
+        int[][] T=new int[s1.length()+1][s2.length()+1];
+        return func(s1,s2,s3,0,0,0,T);
     }
-    boolean func(String s1,String s2,String s3,int i,int j,int k){
+    boolean func(String s1,String s2,String s3,int i,int j,int k,int[][] T){
         if(k==s3.length()){
             return true;
         }
-        if(i!=s1.length() && s1.charAt(i)==s3.charAt(k)){
-            if(func(s1,s2,s3,i+1,j,k+1)){
+        if(i!=s1.length() && s3.charAt(k)==s1.charAt(i)){
+            if(T[i][j]==-1){
+                return false;
+            }
+            if(T[i][j]==1){
+                return true;
+            }
+            if(func(s1,s2,s3,i+1,j,k+1,T)){
+                T[i+1][j]=1;
                 return true;
             }
         }
         if(j!=s2.length() && s2.charAt(j)==s3.charAt(k)){
-            if(func(s1,s2,s3,i,j+1,k+1)){
+            if(T[i][j]==-1){
+                return false;
+            }
+            if(T[i][j]==1){
+                return true;
+            }
+            if(func(s1,s2,s3,i,j+1,k+1,T)){
+                T[i][j+1]=1;
                 return true;
             }
         }
+        T[i][j]=-1;
         return false;
     }
 }
