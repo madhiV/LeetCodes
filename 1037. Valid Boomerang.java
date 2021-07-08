9040d36c5f280420365dee279bd69fc0a49943ef
Madhivadhanan B R <72497080+madhiV@users.noreply.github.com>
2021-07-08 12:50:22 +0530

    Create 1037. Valid Boomerang.java

---
+++ 1037. Valid Boomerang.java
@@ -0,0 +1,9 @@
+class Solution {
+    public boolean isBoomerang(int[][] points) {
+        //Area of the triangle formed from the given points
+        if(((points[0][0]-points[1][0])*(points[0][1]-points[2][1]))-((points[0][1]-points[1][1])*(points[0][0]-points[2][0]))==0){
+            return false;
+        }
+        return true;
+    }
+}
