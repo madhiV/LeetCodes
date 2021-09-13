493e6a27c9ba480b7e0faefa2b211153b6d3a9ef
Madhivadhanan B R <72497080+madhiV@users.noreply.github.com>
2021-09-13 11:07:49 +0530

    Create 283. Move Zeroes.java

---
+++ 283. Move Zeroes.java
@@ -0,0 +1,15 @@
+class Solution {
+    public void moveZeroes(int[] nums) {
+        int k=0;
+        for(int i=0;i<nums.length;i++){
+            if(nums[i]!=0){
+                nums[k]=nums[i];
+                k++;
+            }
+        }
+        while(k!=nums.length){
+            nums[k]=0;
+            k++;
+        }
+    }
+}
