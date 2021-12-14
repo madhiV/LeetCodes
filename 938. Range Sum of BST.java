f2dfa4488d062db163e1f7fbc26ff84d3b1e0561
Madhivadhanan B R <72497080+madhiV@users.noreply.github.com>
2021-12-14 10:57:18 +0530

    Create 938. Range Sum of BST.java

---
+++ 938. Range Sum of BST.java
@@ -0,0 +1,22 @@
+class Solution {
+    public int rangeSumBST(TreeNode root, int low, int high) {
+        if(root==null){
+            return 0;
+        }
+        if(root.val<=high && root.val>=low){
+            return root.val+rangeSumBST(root.left,low,high)+rangeSumBST(root.right,low,high);
+        }
+        if(root.val<low){
+            return rangeSumBST(root.right,low,high);
+        }
+        else if(root.val>high){
+            return rangeSumBST(root.left,low,high);
+        }
+        else if(root.val==low){
+            return root.val+rangeSumBST(root.right,low,high);
+        }
+        else{
+            return root.val+rangeSumBST(root.left,low,high);
+        }
+    }
+}
